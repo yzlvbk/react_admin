1041c63f5aba7fc408c6b50dffd864dad573d3d3
吕冲 <1915156922@qq.com>
2020-08-23 11:23:30 +0800

    完成导航菜单功能

---
+++ src/components/left-nav/left-nav.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './left-nav.less'
+import logo from '../../assets/image/logo.png'
+import { Link, withRouter } from 'react-router-dom'
+import { Menu, Button } from 'antd';
+import menuList from '../../config/menuConfig'
+
+const { SubMenu } = Menu;
+/* 
+左侧导航的组件
+*/
+class LeftNav extends Component {
+
+    /* 
+    根据menu的数据数组动态生成对应的标签数组
+    */
+    getMenuNodes = (menuList) => {
+        return menuList.map((item) => {
+            if(!item.children) {
+               return (
+                <Menu.Item key={item.key}>
+                    <Link to={item.key}>{item.title}</Link>
+                </Menu.Item>
+               )
+            }else {
+                // 得到当前请求的路由路径
+                const path = this.props.location.pathname
+
+                const cItem = item.children.find((cItem) => cItem.key === path)
+                // 如果存在，说明item'的子列表需要展开
+                if(cItem) {
+                    this.openKey = item.key
+                }
+                return (
+                    <SubMenu key={item.key} title={item.title}>
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu>
+                )
+            }
+        })
+    }
+
+    /* 
+    在第一次render()之前执行一次
+    为第一个render()准备数据(同步的)
+    */
+    componentWillMount() {
+        this.menuNodes = this.getMenuNodes(menuList)
+    }
+
+    render() {
+
+        // 得到当前请求的路由路径
+        const path = this.props.location.pathname
+        // 得到需要打开菜单项的key
+        const openKey = this.openKey
+
+        return (
+            <div className="left-nav">
+                <Link to='/'>
+                    <header className="left-nav-header">
+                        <img src={logo} alt=""/>
+                        <h2>后台管理</h2>
+                    </header>
+                </Link>
+
+                <Menu
+                    selectedKeys={[path]}
+                    defaultOpenKeys={[openKey]}
+                    mode="inline"
+                    theme="dark"
+                    >
+                    {this.menuNodes}
+
+                </Menu>
+                
+                
+            </div>
+        );
+    }
+}
+export default withRouter(LeftNav)
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import memoryUtils from '../../utils/memoryUtils'
-import { Redirect } from 'react-router'
+import { Redirect, Route, Switch } from 'react-router'
+import { Layout } from 'antd';
+import LeftNav from '../../components/left-nav/left-nav'
+import Header from '../../components/header/header'
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
+
+const {  Footer, Sider, Content } = Layout;
 
 export default class Admin extends Component {
     render() {
@@ -12,9 +25,29 @@ export default class Admin extends Component {
 
         }
         return (
-            <div>
-                hello {user.username}
-            </div>
+            <Layout style={{height: '100%'}}>
+                <Sider>
+                    <LeftNav></LeftNav>
+                </Sider>
+                <Layout>
+                    <Header></Header>
+                    <Content style={{backgroundColor:"#fff"}}>
+                        <Switch>
+                            <Route path='/home' component={Home}></Route>
+                            <Route path='/category' component={Category}></Route>
+                            <Route path='/product' component={Product}></Route>
+                            <Route path='/role' component={Role}></Route>
+                            <Route path='/user' component={User}></Route>
+                            <Route path='/charts/bar' component={Bar}></Route>
+                            <Route path='/charts/line' component={Line}></Route>
+                            <Route path='/charts/pie' component={Pie}></Route>
+                            <Redirect to="/home" />
+                        </Switch>
+
+                    </Content>
+                    <Footer style={{textAlign: 'center', color: '#ccc'}}>Footer</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
